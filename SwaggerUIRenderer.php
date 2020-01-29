e94303d6e453ee8c90c021506db2d04bb907883a
POCIFIK <pocific.i@gmail.com>
2020-01-29 14:46:06 +0300

    Update SwaggerUIRenderer.php

--- SwaggerUIRenderer.php
+++ SwaggerUIRenderer.php
@@ -19,7 +19,7 @@ class SwaggerUIRenderer extends Action
     /**
      * @var string base swagger template
      */
-    public $view = '@vendor/yii2mod/yii2-swagger/views/index';
+    public $view = '@vendor/pocifik/yii2-swagger/views/index';
 
     /**
      * @var null|string|false the name of the layout to be applied to this controller's views.
